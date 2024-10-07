be8cf567e0491890556365e1e0c4a187777eb249
yongjun2.kwon <yongjun2.kwon@lge.com>
2024-10-07 15:58:16 +0900

    "first commit"

---
+++ src/renderer/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import Main from './components/Main.vue'
+
+window.electronAPI.sendMessage('Hello from App.vue!');
+</script>
+
+<template>
+  <Main/>
+</template>
+
+<style>
+html {
+  overflow-y: auto !important;
+  user-select: none;
+}
+html,
+body {
+  width: 100%;
+  height: 100%;
+}
+/* Do not force capitalization of button text */
+.v-btn {
+  text-transform: unset !important;
+}
+</style>
---
+++ src/renderer/components/Main.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <splitpanes class="default-theme">
+      <pane>left</pane>
+      <pane>
+        <splitpanes horizontal>
+          <pane>top</pane>
+          <pane>main</pane>
+          <pane>bottom</pane>
+        </splitpanes>
+      </pane>
+      <pane>right</pane>
+    </splitpanes>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { Splitpanes, Pane } from 'splitpanes'
+import 'splitpanes/dist/splitpanes.css'
+
+defineProps({
+
+})
+
+</script>
+
+<style>
+.splitpanes__pane {
+  box-shadow: 0 0 3px rgba(0, 0, 0, .2) inset;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+// GENERAL STYLES.
+html, body, #app {height: 100%;margin: 0;}
+body {
+  font-family: Helvetica, Arial, sans-serif;
+  color: rgba(255, 255, 255, 0.8);
+  font-size: 5em;
+}
+
+// documentation link.
+p {
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  color: #666;
+  z-index: 10;
+  font-size: 12px;
+
+  a {color: inherit;}
+}
+
+</style>
